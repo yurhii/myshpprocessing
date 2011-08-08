5fe903f8e7505dc2bbeb2065acebcb67c3e46b67
lfbarba <lfbarba@8add547e-bb3e-11dd-bba1-afcbec1a1e1f>
2011-08-08 23:47:45 +0000

    cambios en dibujado de nivel socioeconómico

--- src/conexion/ConexionMySQL.php
+++ src/conexion/ConexionMySQL.php
@@ -2,8 +2,17 @@
 class ConexionMySQL{
 	var $dtb;
 	function ConexionMySQL($database = "", $server = "macpro.local:8889", $username = "root", $password = "root"){
-		$this->dtb = mysql_connect($server, $username, $password) or die("connection failed");
-		mysql_select_db($database, $this->dtb) or die( "Unable to select database");
+		if($database == ""){
+	 		$database = $this->database;
+		}
+		$servidor = $_SERVER["SERVER_NAME"];	
+		if(strpos($servidor, "mapware") == false){
+			$this->dtb = mysql_connect("localhost:8889", "root", "root") or die("connection failed");
+			mysql_select_db($database, $this->dtb) or die( "Unable to select database");
+		}else{
+			$dtb = mysql_connect("localhost", "lfbarba2_main", "samahil210") or die("no connection to db");
+			mysql_select_db("lfbarba2_MapWare", $this->dtb) or die("no db selected");
+		}
 	}
 }
 ?>
